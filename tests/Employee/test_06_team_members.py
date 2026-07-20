037777ab46f1c6d270acf6c8b27d832097cddcbd
sangili-boopathi-brilyant <sangili.boopathi@brilyant.com>
2026-07-20 16:10:11 +0530

    Created Script for view and filter the team_members and indirect_employees file

---
+++ tests/Employee/test_06_team_members.py
@@ -0,0 +1,58 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+from test_01_login import login
+
+
+def test_team_members(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(
+        channel="chrome", headless=False, args=["--start-maximized"]
+    )
+
+    context = browser.new_context(no_viewport=True)
+    page = context.new_page()
+
+    login(page)
+
+    # Navigate to Employee module
+    page.goto("https://qa.hrmgenie.outstrive.co/employee/general")
+    page.get_by_role("button", name="Team's Members").click()
+
+    # Filter by Designation
+    page.get_by_role("combobox").filter(has_text="All Designations").click()
+    page.get_by_label("Senior FP&A Analyst").get_by_text("Senior FP&A Analyst").click()
+
+    # Filter by Department
+    page.get_by_role("combobox").filter(has_text="All Departments").click()
+    page.get_by_label("Finance").get_by_text("Finance").click()
+
+    # Filter by All Location
+    page.get_by_role("combobox").filter(has_text="All Locations").click()
+    page.get_by_role("option", name="Bangalore").click()
+
+    # Search
+    page.get_by_role("textbox", name="Search...").click()
+    page.get_by_role("textbox", name="Search...").fill("adfas")
+
+    # Resetting the filters
+    page.get_by_role("button", name="Reset Filters").click()
+
+    # Get the data 
+    cell_lines = (
+        page.locator("tbody tr").first.locator("td").nth(1).inner_text().split("\n")
+    )
+    print("Cell Lines ----> ", cell_lines)
+    employee_name = cell_lines[2].strip()
+    print("Employee Name ----> ", employee_name)
+
+    # Clicks on the employee name
+    page.locator("tbody tr").first.locator("td").first.click()
+
+    # Verifies if the employee name is visible
+    expect(page.get_by_role("heading", name=employee_name)).to_be_visible()
+
+    context.close()
+    browser.close()
+
+
+if __name__ == "__main__":
+    with sync_playwright() as playwright:
+        test_team_members(playwright)
---
+++ tests/Employee/test_07_indirect_reportees.py
@@ -0,0 +1,53 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+from test_01_login import login
+
+
+def test_indirect_reportees(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(
+        channel="chrome", headless=False, args=["--start-maximized"]
+    )
+
+    context = browser.new_context(no_viewport=True)
+    page = context.new_page()
+
+    login(page)
+
+    # Navigate to Employee module
+    page.goto("https://qa.hrmgenie.outstrive.co/employee/general")
+    page.get_by_role("button", name="Indirect Reportees").click()
+
+    # Filter by Designation
+    page.get_by_role("combobox").filter(has_text="All Designations").click()
+    page.get_by_role("option", name="Senior FP&A Analyst").click()
+
+    # Filter by Department
+    page.get_by_role("combobox").filter(has_text="All Departments").click()
+    page.get_by_role("option", name="Finance").click()
+
+    # Filter by All location
+    page.get_by_role("combobox").filter(has_text="All Locations").click()
+    page.get_by_role("option", name="Kerala").click()
+
+    # Resetting the filters
+    page.get_by_role("button", name="Reset Filters").click()
+
+    # Get the Data
+    cell_lines = (
+        page.locator("tbody tr").first.locator("td").nth(1).inner_text().split("\n")
+    )
+    print("Cell Lines ----> ", cell_lines)
+    employee_name = cell_lines[2].strip()
+    print("Employee Name ----> ", employee_name)
+
+    # Clicks on the employee name
+    page.locator("tbody tr").first.locator("td").first.click()
+
+    # Verifies if the employee name is visible
+    expect(page.get_by_role("heading", name=employee_name)).to_be_visible()
+    context.close()
+    browser.close()
+
+
+if __name__ == "__main__":
+    with sync_playwright() as playwright:
+        test_indirect_reportees(playwright)
